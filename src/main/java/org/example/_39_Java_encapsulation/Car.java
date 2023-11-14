c8648ef4e775cd689bf9e227fd558db35d88c69e
Siddhant Bali <siddhant22496@iiitd.ac.in>
2023-11-14 18:04:47 +0530

    etn

---
+++ src/main/java/org/example/_39_Java_encapsulation/Car.java
@@ -0,0 +1,44 @@
+package org.example._39_Java_encapsulation;
+
+//***********************************************
+public class Car {
+
+
+    private String make;
+    private String model;
+    private int year;
+
+    Car(String make, String model, int year) {
+        this.setMake(make);
+        this.setModel(model);
+        this.setYear(year);
+    }
+
+//private stuff cannot be accessed by normal way ,they need getters and setters,LIKE BELOW,ELSE IT WILL BE NOT VISIBLE
+    //GETTERS
+    public String getMake() {
+        return this.make;
+    }
+
+    public String getModel() {
+        return this.model;
+    }
+
+    public int getYear() {
+        return this.year;
+    }
+
+    //SETTERS
+    public void setMake(String make) {
+        this.make = make;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+}
